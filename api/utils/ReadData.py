8883e3479b03363981b4e70ae9f4bc1c43f3adac
wangluyan <wangluyan128>
2021-03-29 17:39:44 +0800

    modify

--- api/utils/ReadData.py
+++ api/utils/ReadData.py
@@ -30,7 +30,7 @@ class ReadData(object):
                     value = table.row_values(nrow)
                     value.pop(3)
                     #配合将每一行转换成元组存储，适应pytest的参数化操作，如不需要可以注释掉value = tuple(value)
-                    if iterv is [] or iterv in table.cell_value(nrow,15):
+                    if iterv is '' or iterv in table.cell_value(nrow,15):
                         value = tuple(value)
                         logger.info(f'{value}')
                         data_list.append(value)
